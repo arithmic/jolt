6d4ff02b62875ac945257631969261b2a5c7adf6
arithmic-anuj <anuj@foliumlabs.net>
2025-05-05 16:18:52 +0530

    measure execution time and constraints in tests

--- jolt-on-chain/circuits/algebra/native/bn254/groups/g1_test.go
+++ jolt-on-chain/circuits/algebra/native/bn254/groups/g1_test.go
@@ -2,10 +2,12 @@ package groups
 
 import (
 	"crypto/rand"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/arithmic/gnark/frontend"
-	"github.com/arithmic/gnark/test"
+	"github.com/arithmic/gnark/frontend/cs/r1cs"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
@@ -38,20 +40,42 @@ func (circuit *G1DoubleCircuit) Define(api frontend.API) error {
 }
 
 func TestCircuitG1Double(t *testing.T) {
-	assert := test.NewAssert(t)
+
+	var circuit G1DoubleCircuit
+	// Compile the circuit into an R1CS
+	start := time.Now()
+	r1cs, err := frontend.Compile(ecc.GRUMPKIN.ScalarField(), r1cs.NewBuilder, &circuit)
+	if err != nil {
+		t.Fatalf("Error compiling circuit: %s", err)
+	}
+	duration := time.Since(start)
+	fmt.Printf("Circuit compiled in: %s\n", duration)
+
+	fmt.Println("number of constraints of G1DoubleCircuit", r1cs.GetNbConstraints())
 
 	var a, c bn254.G1Affine
 
 	a = RandomG1Affine()
 	c.Double(&a)
 
-	witness := G1DoubleCircuit{
+	assignment := &G1DoubleCircuit{
 		A: FromG1Affine(&a),
 		C: FromG1Affine(&c),
 	}
 
-	err := test.IsSolved(&G1DoubleCircuit{}, &witness, ecc.GRUMPKIN.ScalarField())
-	assert.NoError(err)
+	// Generate witness
+	start_witness := time.Now()
+	witness, err := frontend.NewWitness(assignment, ecc.GRUMPKIN.ScalarField())
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err_1 := r1cs.Solve(witness)
+	if err_1 != nil {
+		fmt.Println("Error solving the r1cs", err_1)
+		return
+	}
+	duration_witness := time.Since(start_witness)
+	fmt.Printf("Witness generated in: %s\n", duration_witness)
 }
 
 type G1AddCircuit struct {
@@ -59,29 +83,50 @@ type G1AddCircuit struct {
 }
 
 func (circuit *G1AddCircuit) Define(api frontend.API) error {
-	g := &G1API{api: api}
+	g := G1API{api: api}
 	result := g.Add(&circuit.A, &circuit.B)
 	g.AssertIsEqual(result, &circuit.C)
 	return nil
 }
 
 func TestCircuitG1Add(t *testing.T) {
-	assert := test.NewAssert(t)
+
+	var circuit G1AddCircuit
+	// Compile the circuit into an R1CS
+	start := time.Now()
+	r1cs, err := frontend.Compile(ecc.GRUMPKIN.ScalarField(), r1cs.NewBuilder, &circuit)
+	if err != nil {
+		t.Fatalf("Error compiling circuit: %s", err)
+	}
+	duration := time.Since(start)
+	fmt.Printf("Circuit compiled in: %s\n", duration)
+
+	fmt.Println("number of constraints of G1AddCircuit", r1cs.GetNbConstraints())
 
 	var a, b, c bn254.G1Affine
-
 	a = RandomG1Affine()
 	b = RandomG1Affine()
 	c.Add(&a, &b)
 
-	witness := G1AddCircuit{
+	assignment := &G1AddCircuit{
 		A: FromG1Affine(&a),
 		B: FromG1Affine(&b),
 		C: FromG1Affine(&c),
 	}
 
-	err := test.IsSolved(&G1AddCircuit{}, &witness, ecc.GRUMPKIN.ScalarField())
-	assert.NoError(err)
+	// Generate witness
+	start_witness := time.Now()
+	witness, err := frontend.NewWitness(assignment, ecc.GRUMPKIN.ScalarField())
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err_1 := r1cs.Solve(witness)
+	if err_1 != nil {
+		fmt.Println("Error solving the r1cs", err_1)
+		return
+	}
+	duration_witness := time.Since(start_witness)
+	fmt.Printf("Witness generated in: %s\n", duration_witness)
 }
 
 type G1ScalarMulCircuit struct {
@@ -97,7 +142,18 @@ func (circuit *G1ScalarMulCircuit) Define(api frontend.API) error {
 }
 
 func TestCircuitG1ScalarMul(t *testing.T) {
-	assert := test.NewAssert(t)
+	
+	var circuit G1ScalarMulCircuit
+	// Compile the circuit into an R1CS
+	start := time.Now()
+	r1cs, err := frontend.Compile(ecc.GRUMPKIN.ScalarField(), r1cs.NewBuilder, &circuit)
+	if err != nil {
+		t.Fatalf("Error compiling circuit: %s", err)
+	}
+	duration := time.Since(start)
+	fmt.Printf("Circuit compiled in: %s\n", duration)
+
+	fmt.Println("number of constraints of G1ScalarMulCircuit", r1cs.GetNbConstraints())
 
 	var a, c bn254.G1Affine
 	a = RandomG1Affine()
@@ -109,12 +165,23 @@ func TestCircuitG1ScalarMul(t *testing.T) {
 
 	c.ScalarMultiplication(&a, b_big)
 
-	witness := G1ScalarMulCircuit{
+	assignment := &G1ScalarMulCircuit{
 		A:   FromG1Affine(&a),
 		Exp: b1,
 		C:   FromG1Affine(&c),
 	}
 
-	err := test.IsSolved(&G1ScalarMulCircuit{}, &witness, ecc.GRUMPKIN.ScalarField())
-	assert.NoError(err)
+	// Generate witness
+	start_witness := time.Now()
+	witness, err := frontend.NewWitness(assignment, ecc.GRUMPKIN.ScalarField())
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err_1 := r1cs.Solve(witness)
+	if err_1 != nil {
+		fmt.Println("Error solving the r1cs", err_1)
+		return
+	}
+	duration_witness := time.Since(start_witness)
+	fmt.Printf("Witness generated in: %s\n", duration_witness)
 }
