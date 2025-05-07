b0e01c18e301781e7c9dd32ef6d583252742b3d4
arithmic-ASHISH <110672988+arithmic-ASHISH@users.noreply.github.com>
2025-05-07 12:48:36 +0530

    add uniform r1cs for n exp

--- jolt-on-chain/circuits/algebra/native/bn254/field_tower/fp12.go
+++ jolt-on-chain/circuits/algebra/native/bn254/field_tower/fp12.go
@@ -10,15 +10,15 @@ type Fp12 struct {
 }
 
 type Ext12 struct {
-	e6 Ext6
+	E6 Ext6
 }
 
 // NewExt12 creates a new instance of Ext12
 func NewExt12(api frontend.API) *Ext12 {
-	return &Ext12{e6: Ext6{e2: Ext2{api: api}}}
+	return &Ext12{E6: Ext6{e2: Ext2{api: api}}}
 }
 
-// This function is required to create an object of Fp12 from an object of E6 provided by gnark-crypto/ecc/bn254/bn254.go/E12.
+// FromE12 This function is required to create an object of Fp12 from an object of E6 provided by gnark-crypto/ecc/bn254/bn254.go/E12.
 // It comes in handy when we want to create random elements of Fp12
 func FromE12(y *bn254.E12) Fp12 {
 	return Fp12{
@@ -29,21 +29,21 @@ func FromE12(y *bn254.E12) Fp12 {
 
 func (e Ext12) One() *Fp12 {
 	return &Fp12{
-		A0: *e.e6.One(),
-		A1: *e.e6.Zero(),
+		A0: *e.E6.One(),
+		A1: *e.E6.Zero(),
 	}
 }
 
 func (e Ext12) Zero() *Fp12 {
 	return &Fp12{
-		A0: *e.e6.Zero(),
-		A1: *e.e6.Zero(),
+		A0: *e.E6.Zero(),
+		A1: *e.E6.Zero(),
 	}
 }
 
 func (e Ext12) Add(x, y *Fp12) *Fp12 {
-	z0 := e.e6.Add(&x.A0, &y.A0)
-	z1 := e.e6.Add(&x.A1, &y.A1)
+	z0 := e.E6.Add(&x.A0, &y.A0)
+	z1 := e.E6.Add(&x.A1, &y.A1)
 	return &Fp12{
 		A0: *z0,
 		A1: *z1,
@@ -53,7 +53,7 @@ func (e Ext12) Add(x, y *Fp12) *Fp12 {
 func (e Ext12) Conjugate(x *Fp12) *Fp12 {
 	var result Fp12
 	result.A0 = x.A0
-	result.A1 = *e.e6.Neg(&x.A1)
+	result.A1 = *e.E6.Neg(&x.A1)
 	return &result
 }
 
@@ -62,23 +62,23 @@ func (e Ext12) Mul(x, y *Fp12) *Fp12 {
 	var a, b, c Fp6
 
 	// Compute a = (x.A0 + x.A1) * (y.A0 + y.A1)
-	a = *e.e6.Add(&x.A0, &x.A1) // a = x.A0 + x.A1
-	b = *e.e6.Add(&y.A0, &y.A1) // b = y.A0 + y.A1
-	a = *e.e6.Mul(&a, &b)       // a = a * b
+	a = *e.E6.Add(&x.A0, &x.A1) // a = x.A0 + x.A1
+	b = *e.E6.Add(&y.A0, &y.A1) // b = y.A0 + y.A1
+	a = *e.E6.Mul(&a, &b)       // a = a * b
 
 	// Compute b = x.A0 * y.A0
-	b = *e.e6.Mul(&x.A0, &y.A0)
+	b = *e.E6.Mul(&x.A0, &y.A0)
 
 	// Compute c = x.A1 * y.A1
-	c = *e.e6.Mul(&x.A1, &y.A1)
+	c = *e.E6.Mul(&x.A1, &y.A1)
 
 	// Compute z.A1 = a - b - c
-	z1 := *e.e6.Sub(&a, &b) // z.A1 = a - b
-	z1 = *e.e6.Sub(&z1, &c) // z.A1 = z.A1 - c
+	z1 := *e.E6.Sub(&a, &b) // z.A1 = a - b
+	z1 = *e.E6.Sub(&z1, &c) // z.A1 = z.A1 - c
 
 	// Compute z.A0 = b + NonResidue * c
-	z0 := *e.e6.MulByNonResidue(&c) // z.A0 = NonResidue * c
-	z0 = *e.e6.Add(&z0, &b)         // z.A0 = z.A0 + b
+	z0 := *e.E6.MulByNonResidue(&c) // z.A0 = NonResidue * c
+	z0 = *e.E6.Add(&z0, &b)         // z.A0 = z.A0 + b
 
 	// Return the result
 	return &Fp12{
@@ -91,25 +91,25 @@ func (e Ext12) Square(x *Fp12) *Fp12 {
 	var c0, c2, c3 Fp6
 
 	// Compute c0 = x.A0 - x.A1
-	c0 = *e.e6.Sub(&x.A0, &x.A1)
+	c0 = *e.E6.Sub(&x.A0, &x.A1)
 
 	// Compute c3 = NonResidue * x.A1 - x.A0
-	c3 = *e.e6.MulByNonResidue(&x.A1) // c3 = NonResidue * x.A1
-	c3 = *e.e6.Neg(&c3)               // c3 = -c3
-	c3 = *e.e6.Add(&x.A0, &c3)        // c3 = x.A0 + c3
+	c3 = *e.E6.MulByNonResidue(&x.A1) // c3 = NonResidue * x.A1
+	c3 = *e.E6.Neg(&c3)               // c3 = -c3
+	c3 = *e.E6.Add(&x.A0, &c3)        // c3 = x.A0 + c3
 
 	// Compute c2 = x.A0 * x.A1
-	c2 = *e.e6.Mul(&x.A0, &x.A1)
+	c2 = *e.E6.Mul(&x.A0, &x.A1)
 
 	// Compute c0 = c0 * c3 + c2
-	c0 = *e.e6.Mul(&c0, &c3) // c0 = c0 * c3
-	c0 = *e.e6.Add(&c0, &c2) // c0 = c0 + c2
+	c0 = *e.E6.Mul(&c0, &c3) // c0 = c0 * c3
+	c0 = *e.E6.Add(&c0, &c2) // c0 = c0 + c2
 
-	z1 := *e.e6.Add(&c2, &c2) // z.A1 = c2 + c2
+	z1 := *e.E6.Add(&c2, &c2) // z.A1 = c2 + c2
 
 	// Compute z.A0 = c0 + NonResidue * c2
-	c2 = *e.e6.MulByNonResidue(&c2) // c2 = NonResidue * c2
-	z0 := *e.e6.Add(&c0, &c2)       // z.A0 = c0 + c2
+	c2 = *e.E6.MulByNonResidue(&c2) // c2 = NonResidue * c2
+	z0 := *e.E6.Add(&c0, &c2)       // z.A0 = c0 + c2
 
 	// Return the result
 	return &Fp12{
@@ -123,20 +123,20 @@ func (e Ext12) Inverse(x *Fp12) *Fp12 {
 
 	var t0, t1, tmp Fp6
 	// Compute t0 = x.A0^2
-	t0 = *e.e6.Square(&x.A0)
+	t0 = *e.E6.Square(&x.A0)
 	// Compute t1 = x.A1^2
-	t1 = *e.e6.Square(&x.A1)
+	t1 = *e.E6.Square(&x.A1)
 	// Compute tmp = NonResidue * t1
-	tmp = *e.e6.MulByNonResidue(&t1)
+	tmp = *e.E6.MulByNonResidue(&t1)
 	// Compute t0 = t0 - tmp
-	t0 = *e.e6.Sub(&t0, &tmp)
+	t0 = *e.E6.Sub(&t0, &tmp)
 	// Compute t1 = t0^-1 (inverse of t0)
-	t1 = *e.e6.Inverse(&t0)
+	t1 = *e.E6.Inverse(&t0)
 	// Compute z.A0 = x.A0 * t1
-	z0 := *e.e6.Mul(&x.A0, &t1)
+	z0 := *e.E6.Mul(&x.A0, &t1)
 	// Compute z.A1 = -(x.A1 * t1)
-	z1 := *e.e6.Mul(&x.A1, &t1)
-	z1 = *e.e6.Neg(&z1)
+	z1 := *e.E6.Mul(&x.A1, &t1)
+	z1 = *e.E6.Neg(&z1)
 	// Return the result
 	return &Fp12{
 		A0: z0,
@@ -146,8 +146,8 @@ func (e Ext12) Inverse(x *Fp12) *Fp12 {
 
 func (e Ext12) Select(condition frontend.Variable, a, b *Fp12) *Fp12 {
 	// Select the components of a and b based on the condition
-	z0 := e.e6.Select(condition, &b.A0, &a.A0)
-	z1 := e.e6.Select(condition, &b.A1, &a.A1)
+	z0 := e.E6.Select(condition, &b.A0, &a.A0)
+	z1 := e.E6.Select(condition, &b.A1, &a.A1)
 
 	return &Fp12{
 		A0: *z0,
@@ -155,14 +155,14 @@ func (e Ext12) Select(condition frontend.Variable, a, b *Fp12) *Fp12 {
 	}
 }
 
-// TODO: Maybe n = 110. Provides enough security and leads to a smaller circuit.
+// Exp TODO: Maybe n = 110. Provides enough security and leads to a smaller circuit.
 func (e Ext12) Exp(x *Fp12, k *frontend.Variable) *Fp12 {
 	const n = 254
-	bits := e.e6.e2.api.ToBinary(*k, n)
+	bits := e.E6.e2.api.ToBinary(*k, n)
 
 	z := e.One()
 	// Perform binary exponentiation
-	for i := n - 1; i >= 0; i-- {
+	for i := 110 - 1; i >= 0; i-- {
 		// Square z
 		z = e.Square(z)
 		// Conditionally multiply z by x if the current bit is 1
@@ -172,6 +172,6 @@ func (e Ext12) Exp(x *Fp12, k *frontend.Variable) *Fp12 {
 }
 
 func (e Ext12) AssertIsEqual(x, y *Fp12) {
-	e.e6.AssertIsEqual(&x.A0, &y.A0)
-	e.e6.AssertIsEqual(&x.A1, &y.A1)
+	e.E6.AssertIsEqual(&x.A0, &y.A0)
+	e.E6.AssertIsEqual(&x.A1, &y.A1)
 }
--- jolt-on-chain/circuits/pcs/pcs_test.go
+++ jolt-on-chain/circuits/pcs/pcs_test.go
@@ -8,9 +8,11 @@ import (
 	cs "github.com/arithmic/gnark/constraint/grumpkin"
 	"github.com/arithmic/gnark/frontend"
 	"github.com/arithmic/gnark/frontend/cs/r1cs"
+	"github.com/arithmic/jolt/jolt-on-chain/circuits/circuits/algebra/native/bn254/field_tower"
 	"github.com/arithmic/jolt/jolt-on-chain/circuits/circuits/algebra/native/bn254/groups"
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
+	bn254_fp "github.com/consensys/gnark-crypto/ecc/bn254/fp"
 	"github.com/consensys/gnark-crypto/ecc/grumpkin/fp"
 	"github.com/consensys/gnark-crypto/ecc/grumpkin/fr"
 	"strconv"
@@ -18,19 +20,117 @@ import (
 	"time"
 )
 
-type UniformCircuit struct {
+type G1ScalarUniformCircuit struct {
 	In  groups.G1Projective `gnark:",public"`
 	Exp frontend.Variable   `gnark:",public"`
 	Acc groups.G1Projective
 }
 
-func (circuit *UniformCircuit) Define(api frontend.API) error {
+func (circuit *G1ScalarUniformCircuit) Define(api frontend.API) error {
 	groupAPI := &groups.G1API{Api: api}
 	groupAPI.Add(&circuit.Acc, groupAPI.ScalarMul(&circuit.In, &circuit.Exp))
 	return nil
 }
 
-func TestUniformCircuit(t *testing.T) {
+type GTUniformCircuit struct {
+	In  field_tower.Fp12  `gnark:",public"`
+	Exp frontend.Variable `gnark:",public"`
+	Acc field_tower.Fp12
+}
+
+func (circuit *GTUniformCircuit) Define(api frontend.API) error {
+	gtAPI := field_tower.NewExt12(api)
+	gtAPI.Mul(&circuit.Acc, gtAPI.Exp(&circuit.In, &circuit.Exp))
+	return nil
+}
+func TestUniformGTExp(t *testing.T) {
+	var a [100]bn254.E12
+	var b [100]bn254_fp.Element
+	//var bBigInt [100]big.Int
+	for idx := 0; idx < 100; idx++ {
+		_, _ = a[idx].SetRandom()
+		_, _ = b[idx].SetRandom()
+		//b[idx].BigInt(&bBigInt[idx])
+	}
+
+	//c.ScalarMultiplication(&a, b1.)
+
+	//var exp [250]fr.Element
+	//for i := 0; i < 250; i++ {
+	//	b_big, _ := rand.Int(rand.Reader, fp.Modulus())
+	//	exp[i].SetBigInt(b_big)
+	//}
+
+	var circuit GTUniformCircuit
+	start := time.Now()
+	uniformConstraints, err := frontend.Compile(ecc.GRUMPKIN.ScalarField(), r1cs.NewBuilder, &circuit)
+	duration := time.Since(start)
+	fmt.Printf("Circuit compilation time 2 : %s\n", duration)
+	_, aCount, bCount, cCount := ExtractConstraints(uniformConstraints)
+	println("aCount:", aCount, "bCount:", bCount, "cCount:", cCount)
+	
+	var frZero, frOne fr.Element
+	frZero.SetZero()
+	frOne.SetOne()
+
+	var one = field_tower.Fp12{A0: field_tower.Fp6{A0: field_tower.Fp2{A0: frOne, A1: frZero},
+		A1: field_tower.Fp2{A0: frZero, A1: frZero},
+		A2: field_tower.Fp2{A0: frZero, A1: frZero}},
+		A1: field_tower.Fp6{A0: field_tower.Fp2{A0: frZero, A1: frZero},
+			A1: field_tower.Fp2{A0: frZero, A1: frZero},
+			A2: field_tower.Fp2{A0: frZero, A1: frZero}}}
+
+	assignment := &GTUniformCircuit{
+		In:  field_tower.FromE12(&a[0]),
+		Exp: fr.Element(b[0]),
+		Acc: one,
+	}
+
+	start = time.Now()
+	witness, _ := frontend.NewWitness(assignment, ecc.GRUMPKIN.ScalarField())
+
+	wit, _ := uniformConstraints.Solve(witness)
+	z := wit.(*cs.R1CSSolution).W
+	println("Witness size : ", len(z))
+
+	var extendZ fr.Vector
+
+	for idx := 0; idx < len(z); idx++ {
+		extendZ = append(extendZ, z[idx])
+	}
+
+	for idx := 0; idx < 100-1; idx++ {
+		zLen := len(z)
+		Acc := field_tower.Fp12{A0: field_tower.Fp6{A0: field_tower.Fp2{A0: z[zLen-12], A1: z[zLen-11]}, A1: field_tower.Fp2{A0: z[zLen-10], A1: z[zLen-9]}, A2: field_tower.Fp2{A0: z[zLen-8], A1: z[zLen-7]}}, A1: field_tower.Fp6{A0: field_tower.Fp2{A0: z[zLen-6], A1: z[zLen-5]}, A1: field_tower.Fp2{A0: z[zLen-4], A1: z[zLen-3]}, A2: field_tower.Fp2{A0: z[zLen-2], A1: z[zLen-1]}}}
+		assignment := &GTUniformCircuit{
+			In:  field_tower.FromE12(&a[idx+1]),
+			Exp: fr.Element(b[idx+1]),
+			Acc: Acc,
+		}
+
+		witness, _ := frontend.NewWitness(assignment, ecc.GRUMPKIN.ScalarField())
+		wit, _ := uniformConstraints.Solve(witness)
+		z = wit.(*cs.R1CSSolution).W
+
+		for idx := 0; idx < len(z); idx++ {
+			extendZ = append(extendZ, z[idx])
+		}
+
+	}
+	duration = time.Since(start)
+	fmt.Printf("Witness generation time 2 : %s\n", duration)
+
+	//fmt.Printf("extendZ: %v\n", extendZ)
+	if err != nil {
+		fmt.Println("Failed to generated witness", err)
+		return
+	}
+
+	fmt.Println("number of constraints ", uniformConstraints.GetNbConstraints())
+
+}
+
+func TestUniformG1Scalar(t *testing.T) {
 	var a [250]bn254.G1Affine
 	for i := 0; i < 250; i++ {
 		a[i] = groups.RandomG1Affine()
@@ -44,7 +144,7 @@ func TestUniformCircuit(t *testing.T) {
 		exp[i].SetBigInt(b_big)
 	}
 
-	var circuit UniformCircuit
+	var circuit G1ScalarUniformCircuit
 	start := time.Now()
 	uniformConstraints, err := frontend.Compile(ecc.GRUMPKIN.ScalarField(), r1cs.NewBuilder, &circuit)
 	duration := time.Since(start)
@@ -53,7 +153,7 @@ func TestUniformCircuit(t *testing.T) {
 	zero.SetZero()
 	one.SetOne()
 
-	assignment := &UniformCircuit{
+	assignment := &G1ScalarUniformCircuit{
 		In: groups.FromG1Affine(&a[0]),
 		Acc: groups.G1Projective{
 			X: zero,
@@ -78,7 +178,7 @@ func TestUniformCircuit(t *testing.T) {
 	for idx := 0; idx < 250-1; idx++ {
 		zLen := len(z)
 		var Acc = groups.G1Projective{X: z[zLen-3], Y: z[zLen-3], Z: z[zLen-3]}
-		assignment := &UniformCircuit{
+		assignment := &G1ScalarUniformCircuit{
 			In:  groups.FromG1Affine(&a[idx+1]),
 			Acc: Acc,
 			Exp: exp[idx+1],
@@ -121,15 +221,15 @@ func PrettyPrintConstraints(constraints []Constraint) {
 	fmt.Println(string(bytes))
 }
 
-func ExtractConstraints(r1cs constraint.ConstraintSystem) []Constraint {
+func ExtractConstraints(r1cs constraint.ConstraintSystem) ([]Constraint, int, int, int) {
 	var outputConstraints []Constraint
+	var aCount, bCount, cCount int
 
 	// Assert to R1CS to get access to R1CS-specific methods
 	nR1CS, ok := r1cs.(constraint.R1CS)
 	if !ok {
-		return outputConstraints // or handle error
+		return outputConstraints, 0, 0, 0 // or handle error
 	}
-
 	constraints := nR1CS.GetR1Cs()
 	for _, r1c := range constraints {
 		singular := Constraint{
@@ -142,20 +242,23 @@ func ExtractConstraints(r1cs constraint.ConstraintSystem) []Constraint {
 			val := nR1CS.CoeffToString(int(term.CID))
 			col := strconv.FormatUint(uint64(term.VID), 10)
 			singular.A[col] = val
+			aCount++
 		}
 		for _, term := range r1c.R {
 			val := nR1CS.CoeffToString(int(term.CID))
 			col := strconv.FormatUint(uint64(term.VID), 10)
 			singular.B[col] = val
+			bCount++
 		}
 		for _, term := range r1c.O {
 			val := nR1CS.CoeffToString(int(term.CID))
 			col := strconv.FormatUint(uint64(term.VID), 10)
 			singular.C[col] = val
+			cCount++
 		}
 
 		outputConstraints = append(outputConstraints, singular)
 	}
 
-	return outputConstraints
+	return outputConstraints, aCount, bCount, cCount
 }
